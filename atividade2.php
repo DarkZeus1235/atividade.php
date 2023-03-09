c2d43f604e8178d67e52d5d52fc3aa9e8767ceb1
JOAO PEDRO FERNANDES DE ALMEIDA <joao.8976@aluno.pr.senac.br>
2023-03-09 11:59:49 -0300

    ATUALIZAÇÃO

---
+++ atividade2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividade2</title>
+</head>
+<body>
+    <?php
+    $valor = 1;
+    $valor2 = 2;
+
+    if($valor > $valor2)
+    echo $valor . " É maior do que " . $valor2;
+
+    else
+    echo $valor2 . " é maior do que  " . $valor;
+    ?>  
+</body>
+</html>
